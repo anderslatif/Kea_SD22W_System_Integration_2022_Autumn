19908bd7e179022015c8df8a97dffc80858f7be5
Anders Latif <andl@kea.dk>
2022-11-01 10:45:47 +0100

    Made updates to the task queue project

--- 14._Redis_Task_Queues/scheduler.py
+++ 14._Redis_Task_Queues/scheduler.py
@@ -1,12 +1,18 @@
 from redis import Redis
 from rq import Queue
 from scheduler_util import perform_task
+import random
 
 queue = Queue(name="mainqueue", connection=Redis())
 
 
-
-
-
 for _ in range(20):
-    queue.enqueue(perform_task)
+    sleep_time = random.randint(1, 5)
+    queue.enqueue(perform_task, sleep_time)
+
+# Additional cool options:
+""" 
+job = queue.enqueue(perform_task, retry=Retry(max=3, interval=[10, 30, 60]))
+job = queue.enqueue_in(timedelta(seconds=1), perform_task)
+job = queue.enqueue_at(datetime(2019, 10, 8, 9, 15), perform_task)
+"""
--- 14._Redis_Task_Queues/scheduler_util.py
+++ 14._Redis_Task_Queues/scheduler_util.py
@@ -1,8 +1,7 @@
-import random
 import time
 
-def perform_task():
+def perform_task(sleep_time=1):
     print("Performing task....")
-    time.sleep(random.randint(1, 5))
+    time.sleep(sleep_time)
     print("Task is done!")
     time.sleep(1)

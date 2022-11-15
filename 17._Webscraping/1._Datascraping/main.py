30f0c44a78d9424402cedc12a450910a1d77aa03
Anders Latif <andl@kea.dk>
2022-11-15 10:37:00 +0100

    Implemented a datascraper for any Wiki page

---
+++ 17._Webscraping/1._Datascraping/main.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+html = requests.get('https://en.wikipedia.org/wiki/List_of_Monty_Python_projects').content
+parsed_html = BeautifulSoup(html, features="lxml")
+
+main_page_content = parsed_html.find('div', {'class': 'mw-parser-output'})
+
+monty_python_projects = {}
+project_type = None
+
+for tag in main_page_content:
+    if tag.name == "h2":
+        project_type = tag.text.replace("[edit]", "")
+        monty_python_projects[project_type] = []
+    elif tag.name == "ul":
+        for list_item in tag.find_all("li"):
+            monty_python_projects[project_type].append(list_item.text)
+    elif tag.name == "table":
+        pass 
+
+
+del monty_python_projects['Notes']
+del monty_python_projects['References']
+del monty_python_projects['Further reading']
+
+from pprint import pprint
+pprint(monty_python_projects)

b137be3f132699de258f6150ff5ec06cb45c32bf
Anders Latif <andl@kea.dk>
2022-12-06 11:52:04 +0100

    Finished graphql

--- 19._graphql/app_movies.js
+++ 19._graphql/app_movies.js
@@ -16,7 +16,7 @@ const actors = [
     { id: 3, name: "Arnold Schwarzenegger" },
 ];
 
-import { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList } from "graphql";
+import { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList, GraphQLNonNull } from "graphql";
 
 const ActorType = new GraphQLObjectType({
     name: "Actor",
@@ -50,6 +50,52 @@ const MovieType = new GraphQLObjectType({
     }
 });
 
+const RootMutationType = new GraphQLObjectType({
+    name: "RootMutationType",
+    fields: {
+        addMovie: {
+            type: MovieType,
+            description: "Create a new movie",
+            args: {
+                title: { type: new GraphQLNonNull(GraphQLString) },
+                actorIds: { type: new GraphQLNonNull(new GraphQLList(GraphQLInt)) }
+            },
+            resolve: (parent, args) => {
+                const foundActors = args.actorIds.map(actorId => 
+                        actors.find(actor => actor.id === actorId)
+                );
+
+                if (foundActors.includes(undefined)) return;
+
+                const movie = {
+                    id: ++movieId,
+                    title: args.title,
+                    actorIds: args.actorIds
+                };
+
+                movies.push(movie);
+                return movie;
+            }
+        },
+        addActor: {
+            type: ActorType,
+            description: "Create a new actor",
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve: (parent, { name }) => {
+                const actor = {
+                    id: ++actorId,
+                    name
+                };
+
+                actors.push(actor);
+                return actor;
+            }
+        }
+    }
+});
+
 const RootQueryType = new GraphQLObjectType({
         name: "RootQueryType",
         fields: {
@@ -62,12 +108,23 @@ const RootQueryType = new GraphQLObjectType({
                 type: new GraphQLList(ActorType),
                 description: "All actors",
                 resolve: () => actors
+            },
+            actorById: {
+                type: ActorType,
+                description: "Get a single actor",
+                args: {
+                    id: { type: new GraphQLNonNull(GraphQLInt) }
+                },
+                resolve: (parent, args) => {
+                    return actors.find(actor => actor.id === args.id);
+                }
             }
         }
 });
 
 const schema = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMutationType
 });
 
 import { graphqlHTTP } from "express-graphql";
